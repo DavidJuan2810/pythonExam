5f383dbfb714d525a75e706e59a7a460c835cfe0
DavidJuan2810 <juandavides120@gmail.com>
2024-12-11 09:27:29 -0500

    actualizacion de router

--- apps/orders/api/routers.py
+++ apps/orders/api/routers.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from apps.orders.api.views import ProductViewSet, OrderViewSet, TableViewSet, ReservaViewSet
 
-router1 = DefaultRouter()
-router1.register(prefix='product', basename='product', viewset= ProductViewSet)
-router1.register(prefix='order',basename='order', viewset=OrderViewSet)
-router1.register(prefix='table', basename='table', viewset=TableViewSet)
-router1.register(prefix='reserva', basename='reserva', viewset=ReservaViewSet)
+router2 = DefaultRouter()
+router2.register(prefix='product', basename='product', viewset= ProductViewSet)
+router2.register(prefix='order',basename='order', viewset=OrderViewSet)
+router2.register(prefix='table', basename='table', viewset=TableViewSet)
+router2.register(prefix='reserva', basename='reserva', viewset=ReservaViewSet)
+
